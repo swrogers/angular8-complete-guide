eed1b72ee1fa49ae8dd74bdc3e64e0df0f93f987
Shane Rogers <swrogers@gmail.com>
2019-11-30 14:51:56 -0600

    Property binding in templates

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,15 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // select by dom-tag
   // selector: '[app-servers]',  // select by attribute
   // selector: '.app-servers',  // select by class
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
