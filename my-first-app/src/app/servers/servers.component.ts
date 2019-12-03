839c975a0db49791de7d0a73b7b4fbdebb93e64c
Shane Rogers <swrogers@gmail.com>
2019-12-03 15:39:22 -0600

    Practice Directives assigment 3 - start

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -16,6 +16,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'no server created';
   serverName = 'test server';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +28,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
     this.serverCreated = true;
   }
