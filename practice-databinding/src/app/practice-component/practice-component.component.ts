fa90a0f13cded249475a7abfc4c0fe2ca246906e
Shane Rogers <swrogers@gmail.com>
2019-12-03 10:50:29 -0600

    Practice databinding done

---
+++ practice-databinding/src/app/practice-component/practice-component.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-component',
+  templateUrl: './practice-component.component.html',
+  styleUrls: ['./practice-component.component.css']
+})
+export class PracticeComponentComponent implements OnInit {
+  userName = '';
+  userNameIsEmpty = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onUpdateUserName(event: any) {
+    if(event.target.value != '') {
+      this.userNameIsEmpty = false;
+    }
+  }
+
+  onResetButtonClick() {
+    this.userName = '';
+  }
+
+}
