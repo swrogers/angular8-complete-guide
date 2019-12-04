0e2fabb79583a1dbe6a322746111d93d9aeb07a7
Shane Rogers <swrogers@gmail.com>
2019-12-04 15:06:08 -0600

    Prefer css file

--- practice-directives/src/app/app.component.ts
+++ practice-directives/src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-    .white {
-      color: white;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   showParagraph = false;
