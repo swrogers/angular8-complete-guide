76fd3ce2fe1ad52257563536a5343e8b8718eae3
Shane Rogers <swrogers@gmail.com>
2019-12-07 14:03:00 -0600

    Binding serverElement to customer property 'element'

--- server-component-app/src/app/app.component.ts
+++ server-component-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'server-component-app';
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
 }
