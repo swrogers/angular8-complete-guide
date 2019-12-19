755e4c4fc3ff10d62433f4c7cd7736f639d1ef68
Shane Rogers <swrogers@gmail.com>
2019-12-19 12:00:31 -0600

    Sending recipe items to shopping list via services

--- recipe-shopper/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-shopper/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,3 +1,5 @@
+import { Ingredient } from './../../shared/ingredient.model';
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
@@ -9,9 +11,15 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  toShoppingList(ingredients: Ingredient[]) {
+    ingredients.forEach((ingredient) => {
+      this.shoppingListService.addIngredient(ingredient);
+    });
   }
 
 }
