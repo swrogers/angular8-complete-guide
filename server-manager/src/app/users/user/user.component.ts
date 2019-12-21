c92b87ddd88d905ab36be1969bb299d221f420c9
Shane Rogers <swrogers@gmail.com>
2019-12-21 11:35:21 -0600

    Routing - Manually unsubscribing from route parameter subscription

--- server-manager/src/app/users/user/user.component.ts
+++ server-manager/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -23,13 +25,17 @@ export class UserComponent implements OnInit {
     // subscribe to the route.params in order to be
     // notified of changes within the changed
     // component itself
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
