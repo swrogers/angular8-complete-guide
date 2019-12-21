08f2989367a908d6c94f9a064a0f86d76b89303c
Shane Rogers <swrogers@gmail.com>
2019-12-21 12:22:55 -0600

    Routing - Retrieving query parameters and fragments

--- server-manager/src/app/servers/servers.service.ts
+++ server-manager/src/app/servers/servers.service.ts
@@ -9,4 +9,21 @@ export class ServersService {
     return this.servers;
   }
 
+  getServer(id: number) {
+    const server = this.servers.find(
+      (s) => {
+        return s.id === id;
+      }
+    );
+    return server;
+  }
+
+  updateServer(id: number, serverInfo: {name: string, status: string}) {
+    const server = this.getServer(id);
+    if (server) {
+      server.name = serverInfo.name;
+      server.status = serverInfo.status;
+    }
+  }
+
 }
