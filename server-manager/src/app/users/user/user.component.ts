f5812477f0c1ae8ecaedcacf1e997489174549d8
Shane Rogers <swrogers@gmail.com>
2019-12-21 11:26:57 -0600

    Routing - Fetching route parameters reactively

--- server-manager/src/app/users/user/user.component.ts
+++ server-manager/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -13,9 +13,23 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
     this.user = {
+      // snapshot's are fine for the initial load, but
+      // don't notify angular if the data changes while
+      // in the same component.
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+
+    // subscribe to the route.params in order to be
+    // notified of changes within the changed
+    // component itself
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
