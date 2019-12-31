7c22d950a389e342df3f3f6f113abd455dacfca9
Shane Rogers <swrogers@gmail.com>
2019-12-31 13:40:15 -0600

    Routing - Guard just the child routes

--- server-manager/src/app/auth-guard.service.ts
+++ server-manager/src/app/auth-guard.service.ts
@@ -3,13 +3,14 @@ import { CanActivate,
         ActivatedRoute,
         ActivatedRouteSnapshot,
         RouterStateSnapshot,
-        Router
+        Router,
+        CanActivateChild
       } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private authService: AuthService,
@@ -32,4 +33,13 @@ export class AuthGuard implements CanActivate {
         }
       );
     }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<boolean> | Promise<boolean> | boolean {
+
+    return this.canActivate(route, state);
+  }
+
 }
