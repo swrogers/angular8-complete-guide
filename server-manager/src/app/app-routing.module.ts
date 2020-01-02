c2421c84e98e56f2cd27ef9f06ca4468c28fda55
Shane Rogers <swrogers@gmail.com>
2020-01-02 13:38:21 -0600

    Routing - control navigation with canDeactivate

--- server-manager/src/app/app-routing.module.ts
+++ server-manager/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { AuthGuard } from './auth-guard.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -22,7 +23,7 @@ const   appRoutes: Routes = [
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     ]
   },
   { path: 'not-found', component: PageNotFoundComponent },
