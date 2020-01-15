ea7e2b92a0efd0f1fbde0d08aea88fa0bb2c9d6f
Shane Rogers <swrogers@gmail.com>
2020-01-15 14:53:01 -0600

    Using Subjects to communicate with services.

--- understanding-observables/src/app/app.component.ts
+++ understanding-observables/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { UserService } from "./user.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'understanding-observables';
+export class AppComponent implements OnInit, OnDestroy {
+  private activeSubscription: Subscription;
+  userActivated = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activeSubscription = this.userService.activatedEmitter.subscribe(
+      didActivate => {
+        this.userActivated = didActivate;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.activeSubscription.unsubscribe();
+  }
 }
