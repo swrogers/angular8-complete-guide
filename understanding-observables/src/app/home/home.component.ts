a139bca9cd0170f0d6e37cccb278dc41979dd71c
Shane Rogers <swrogers@gmail.com>
2020-01-15 14:06:02 -0600

    Using rxjs operators in a pipe.

--- understanding-observables/src/app/home/home.component.ts
+++ understanding-observables/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { interval, Subscription, Observable } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -29,24 +30,48 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstSub = customIntervalObservable.subscribe(
-      // Callback to perform work on the information returned
-      // from the observable
-      data => {
-        console.log(data);
-      },
-      // Callback to run should the observable throw an
-      // error
-      error => {
-        console.log(error);
-        alert(error.message);
-      },
-      // Callback that runs when the observable completes.
-      // This does not run after an error.
-      () => {
-        console.log("Completed!");
-      }
-    );
+    // Use a pipe to change the output from the observable,
+    // then subscribe to that version of the observable.
+    this.firstSub = customIntervalObservable
+      .pipe(
+        filter((data: number) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log("Completed!");
+        }
+      );
+
+    // this.firstSub = customIntervalObservable.subscribe(
+    //   // Callback to perform work on the information returned
+    //   // from the observable
+    //   data => {
+    //     console.log(data);
+    //   },
+    //   // Callback to run should the observable throw an
+    //   // error
+    //   error => {
+    //     console.log(error);
+    //     alert(error.message);
+    //   },
+    //   // Callback that runs when the observable completes.
+    //   // This does not run after an error.
+    //   () => {
+    //     console.log("Completed!");
+    //   }
+    // );
   }
 
   ngOnDestroy(): void {
