daa422fa94f3633ff021884d9e968902fd1af7e5
Shane Rogers <swrogers@gmail.com>
2020-01-30 10:48:44 -0600

    Form Practice - completed

--- practice-forms/src/app/app.component.ts
+++ practice-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) subForm: NgForm;
+  defaultSubscription = 'advanced';
+  submitted = false;
+  data = {
+    emailAddress: '',
+    password: '',
+    subscription: ''
+  };
+
   title = 'practice-forms';
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.subForm.value.emailAddress);
+    const submittedData = this.subForm.value;
+
+    this.data.emailAddress = submittedData.emailAddress;
+    this.data.password = submittedData.password;
+    this.data.subscription = submittedData.subscription;
+  }
 }
