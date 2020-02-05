7ce604be2f0c75e47794b98f2f52b381fd1b0bce
Shane Rogers <swrogers@gmail.com>
2020-02-05 14:18:06 -0600

    Reactive Forms - Custom Validators
    Creating a custom validator. Remember the need to bind "this", since Angular
    will be calling the validation function and not the class itself.

--- form-handling-reactive/src/app/app.component.ts
+++ form-handling-reactive/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -31,5 +32,12 @@ export class AppComponent implements OnInit {
 
   get controls() {
     return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenNames(control: FormControl) : {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
