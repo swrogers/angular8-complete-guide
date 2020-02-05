126f2802142b0f00b6da6fe6c321657226e25d40
Shane Rogers <swrogers@gmail.com>
2020-02-05 15:41:54 -0600

    Reactive Forms Practce - Initial setup

---
+++ practice-forms-reactive/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'practice-forms-reactive';
+  projectForm: FormGroup;
+
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+
+  }
+}
