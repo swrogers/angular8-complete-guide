06dc9c580fc90f957c93222221b5c03b719069e6
Shane Rogers <swrogers@gmail.com>
2020-02-06 15:20:11 -0600

    Course Project - Template Driven Forms
    Removed the viewchild access for the shopping list form, instead passing in the
    form reference to the addbuttonclick method on the form.

--- recipe-shopper/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-shopper/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,6 @@
 import { ShoppingListService } from './../shopping-list.service';
 import { Ingredient } from './../../shared/ingredient.model';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,25 +9,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('f', {static: true}) shoppingForm: NgForm;
-  @ViewChild('nameInput', {static: true}) nameInput: ElementRef;
-  @ViewChild('amountInput', {static: true}) amountInput: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddButtonClicked() {
+  onAddButtonClicked(form: NgForm) {
     this.shoppingListService.shoppingItemToAdd.next(
-      //new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value)
-      new Ingredient(this.shoppingForm.value.name, this.shoppingForm.value.amount)
+      new Ingredient(form.value.name, form.value.amount)
     );
   }
 
   onClearButtonClicked() {
-    this.nameInput.nativeElement.value = '';
-    this.amountInput.nativeElement.value = '';
+    this.shoppingForm.reset();
   }
 
 }
